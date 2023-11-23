53f973beb1e6e876a713e41967c6fc805f88a2da
francis <clyderitchie@yahoo.com>
2023-11-22 20:05:54 -0800

    Finished with the  index.js file in the models and working on making some seeds for the blog spot now

---
+++ modules/index.js
@@ -0,0 +1,14 @@
+const User = require('./user');
+const Post = require('./post');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+Model.exports = { User, Post };

bb7d7f8c3e0221f06533a61493e4b888dee38ccc
francis <clyderitchie@yahoo.com>
2023-11-26 20:42:06 -0800

    Added the main.handlebars in the layouts/views folder to access the homeRoutes in the controllors folder to render the home page which will act like a homepage for Twitter(X) and Fscebook where you see everyones post they have created from newest to oldest

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,23 @@ const router = require('express').Router();
 const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
-
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: {
+                model: User,
+                attributes: ['name']
+            }
+        });
+        const posts = postData.map(p => p.get({ plain: true }))
+        res.render('post', {
+            posts,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json(err.message);
+    }
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -29,7 +30,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
